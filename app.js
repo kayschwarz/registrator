f099df90a3b304781c978f6ae4ceb2f08bdcef9e
Max F. Albrecht <1@178.is>
2013-02-27 17:03:46 +0100

    implement simple database, plus route for testing it

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-var flatiron = require('flatiron'),
-    path = require('path'),
-    app = flatiron.app;
+var flatiron  = require('flatiron'),
+    path      = require('path'),
+    app       = flatiron.app,
+    Database  = require('simple-js'), // database: json in fs
+    db        = new Database("db"); // a folder "db"
 
+// app: config
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
+// app: http plugin
 app.use(flatiron.plugins.http);
 
+// ROUTER ////////////////////////////////////////////
+
+// homepage
 app.router.get('/', function () {
   this.res.json({ 'hello': 'world' })
+});
+
+// get knoten info
+app.router.get('/knoten/:number', function (number) {
+  var http = this;
+  db.get(number, function(err, obj) {
+    // respond with error, if any
+    if (err) {
+      app.log.debug(err);
+      
+    } else { // if no error
+      // build data
+      var data = {
+        // the number property is our current value
+        "number": number,
+        // just take the values we want from the db result
+        // (so we could add private metadata)
+        "mac": obj.mac,
+        "pass": obj.pass
+      };
+      // send it
+      http.res.json(data);
+    }
+  });
 });
 
 app.start(3000);
