57954afbfce89e3eefd40dcdab066f25784398ee
Max F. Albrecht <1@178.is>
2013-02-27 22:07:54 +0100

    heartbeat: check the supplied client data ^^

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.router.get('/knoten', function () {
   });
 });
 
+
 // get single knoten info
 app.router.get('/knoten/:number', function (number) {
   var http = this;
@@ -78,13 +79,12 @@ app.router.get('/knoten/:number', function (number) {
   });
 });
 
-// PUT single knoten
-app.router.get('/put/knoten/:number/:mac/:pass', function (number) {
+// Auto-Reg: knoten without number, but mac pass
+app.router.get('/put/knoten/:mac/:pass', function (mac, pass) {
   var http = this,      
       data = {};
   
-  console.log(require('eyes').inspect(this.req.query));
-      
+  
   db.get(number, function(err, knoten) {
     // respond with error, if any
     if (err) {
@@ -139,9 +139,92 @@ app.router.get('/put/knoten/:number/:mac/:pass', function (number) {
         }
       });
     }
-  });
-    
-  
+  });  
+});
+
+// PUT single knoten with data (auto-reg or heartbeat)
+app.router.get('/put/knoten/:number/:mac/:pass', function (number, mac, pass) {
+  var http = this,      
+      data = {};
+        
+  db.get(number, function(err, knoten) {
+    // respond with error, if any
+    if (err) {      
+      // we just assume a 404
+      var e = {
+        "status": 404,
+        "msg": "Not Found"
+      };
+      http.res.writeHead(e.status, e.msg);
+      http.res.json(e);
+      
+    } else { // if no error, continue updating
+      
+      if (mac !== knoten.mac) {
+        // wrong pass: Error 400 Bad Request
+        var e = {
+          "status": 401,
+          "msg": "Wrong $MAC"
+        };
+        http.res.writeHead(e.status, e.msg);
+        http.res.json(e);
+        
+      } 
+      
+      if (pass !== knoten.pass) {
+        // wrong pass: Error 401 Unauthorized
+        var e = {
+          "status": 401,
+          "msg": "Wrong $PASS"
+        };
+        http.res.writeHead(e.status, e.msg);
+        http.res.json(e);
+        
+      }
+      
+      // it seems all is well! let's do the heartbeat:
+      
+      // timestamp the knoten
+      knoten.last_seen = new Date().getTime();
+      
+      // save knoten back to db
+      db.save(number, knoten, function(err) {
+        
+        if (err) {
+          
+          // db error at this stage is our fault
+          var e = {
+            "status": 500,
+            "msg": "Server Error"
+          };
+          
+          http.res.writeHead(e.status, e.msg);
+          http.res.json(e);
+          
+          
+        } else {
+          
+          // build answer
+          var a = {
+            "status": 200,
+            "msg": "ok",
+            "result": {
+              // number is still the number
+              "number": number,
+              // we pick the just values we want from the db result
+              "mac": knoten.mac,
+              "last_seen": knoten["last_seen"]
+            }
+          };
+          
+          // send it
+          http.res.json(a);
+          
+        }
+      });      
+
+    }
+  });  
 });
 
 
