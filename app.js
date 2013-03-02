1f0c14dab412f2e844056c3d5201f1617f24f79d
Max F. Albrecht <1@178.is>
2013-03-02 05:23:18 +0100

    cleanup

--- app.js
+++ app.js
@@ -14,54 +14,45 @@ app.use(flatiron.plugins.http);
 app.use(require("./lib/errors"), { "style": "http" } );
 app.use(require("./lib/register"), { "networks": ['ffweimar'] } );
 
-// ROUTER ////////////////////////////////////////////
+// # ROUTER
 
-// HOMEPAGE
+// ## HOMEPAGE
 app.router.get('/', function () {
   this.res.json({ 'RTFM': 'https://github.com/eins78/registrator/' })
 });
 
-// LIST: GET /knoten
-app.router.get('/GET/knoten', function () {
+// ## LIST: GET /knoten
+var getAll = function () {
   var http = this;
   
   app.register.getAll(function(err, res) {
     http.res.json(err || res);
   });
   
-});
-app.router.get('/knoten', function () {
-  var http = this;
-  
-  app.register.getAll(function(err, res) {
-    http.res.json(err || res);
-  });
-  
-});
+};
 
-// CHECK/INFO: GET /knoten/number
-app.router.get('/GET/knoten/:number', function (number) {
+app.router.get('/knoten', getAll);
+app.router.get('/GET/knoten', getAll);
+
+// ## CHECK/INFO: GET /knoten/number
+var getKnoten = function (number) {
   var http = this;
   
   app.register.get(number, function(err, res) {
     http.res.json(err || res);
   });
-});
-app.router.get('/knoten/:number', function (number) {
-  var http = this;
-  
-  app.register.get(number, function(err, res) {
-    http.res.json(err || res);
-  });
-});
+};
+
+app.router.get('/knoten/:number', getKnoten(number));
+app.router.get('/GET/knoten/:number', getKnoten(number));
 
 // TODO: get knoten property
 // app.router.get('/GET/knoten/:number/:property', function (number) {
 //   app.register.check(number, property, this);
 // });
 
-// AUTOREGISTER: POST knoten, needs mac and pass
-app.router.get('/POST/knoten', function () {
+// ## AUTOREGISTER: POST knoten, needs mac and pass
+var postKnoten = function () {
   var http = this, 
   
   mac = http.req.query.mac || null,
@@ -71,11 +62,14 @@ app.router.get('/POST/knoten', function () {
     http.res.json(err || res);
   });
   
-});
+};
 
-// HEARTBEAT: PUT knoten/number, needs mac and pass
+app.router.post('/knoten', postKnoten(number));
+app.router.get('/POST/knoten', postKnoten(number));
+
+// ## HEARTBEAT: PUT knoten/number, needs mac and pass
 // - special: if number has no pass, set to given pass
-app.router.get('/PUT/knoten/:number', function (number) {
+var putKnoten = function (number) {
   var http = this,
   
   mac = http.req.query.mac || null,
@@ -86,12 +80,18 @@ app.router.get('/PUT/knoten/:number', function (number) {
     http.res.json(err || res);
   });
   
-});
+};
 
-// TIMESTAMP: GET /time
-app.router.get('/GET/time', function () {
+app.router.put('/knoten/:number', putKnoten(number));
+app.router.get('/PUT/knoten/:number', putKnoten(number));
+
+// ## TIMESTAMP: GET /time
+var getTime = function () {
   this.res.json({ 'now': new Date().getTime() })
-});
+};
+
+app.router.get('/time', getTime());
+app.router.get('/GET/time', getTime());
 
 // start http server on configured port
 app.start(app.config.get('port'));
